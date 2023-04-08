f054cba24108598401bb353b4641a7186d1a6bed
Takumi Comeau <tk565319@dal.ca>
2023-04-07 23:38:53 -0300

    created logic to get to grass from pokecenter

---
+++ heal_return.py
@@ -0,0 +1,73 @@
+import time
+
+import pyautogui
+import pydirectinput
+import requests
+from PIL import Image
+
+import random_breaks
+
+
+outside_building = Image.open(requests.get("https://raw.githubusercontent.com/"
+                                           "T3tsuo/AmuletFarming/main/location/outside_building.png", stream=True).raw)
+
+
+def leave_building():
+    pydirectinput.keyDown("down")
+    time.sleep(random_breaks.leave_building())
+    pydirectinput.keyUp("down")
+    # while cannot find outside, keep on waiting
+    is_outside = False
+    while is_outside is False:
+        # if image recognition detects that we left the building
+        if pyautogui.locateOnScreen(outside_building, confidence=0.8) is not None:
+            # then we are outside
+            print("Left Building")
+            is_outside = True
+            time.sleep(0.5)
+        else:
+            time.sleep(0.5)
+
+
+def go_to_grass():
+    # hop on bike
+    pydirectinput.press("1")
+    print("Bicycle")
+    time.sleep(random_breaks.input_break())
+    # go left
+    pydirectinput.keyDown("left")
+    time.sleep(random_breaks.three_blocks())
+    pydirectinput.keyUp("left")
+    time.sleep(random_breaks.input_break())
+    # go down
+    pydirectinput.keyDown("down")
+    time.sleep(random_breaks.eleven_blocks())
+    pydirectinput.keyUp("down")
+    time.sleep(random_breaks.input_break())
+    # go right
+    pydirectinput.keyDown("right")
+    time.sleep(random_breaks.seven_blocks())
+    pydirectinput.keyUp("right")
+    time.sleep(random_breaks.input_break())
+    # look down
+    pydirectinput.press("down")
+    time.sleep(random_breaks.input_break())
+    # use cut
+    pydirectinput.keyDown("z")
+    time.sleep(random_breaks.use_cut_break())
+    pydirectinput.keyUp("z")
+    time.sleep(random_breaks.input_break())
+    # go to grass
+    pydirectinput.keyDown("down")
+    time.sleep(random_breaks.to_grass_break())
+    pydirectinput.keyUp("down")
+    time.sleep(random_breaks.input_break())
+
+
+def run():
+    leave_building()
+    go_to_grass()
+
+
+time.sleep(2)
+run()
---
+++ random_breaks.py
@@ -0,0 +1,66 @@
+from random import random
+
+
+def to_grass_break():
+    # 1.58 - 1.6
+    return random() * 0.02 + 1.58
+
+
+def use_cut_break():
+    # 4 - 5 seconds
+    return random() * 1 + 4
+
+
+def seven_blocks():
+    # 0.53 - 0.55 seconds
+    return random() * 0.02 + 0.53
+
+
+def eleven_blocks():
+    # from 0.89 to 0.91 seconds
+    return random() * 0.02 + 0.89
+
+
+def three_blocks():
+    # from 0.23 to 0.24 seconds
+    return random() * 0.02 + 0.23
+
+
+def leave_building():
+    # 1.1 seconds to 1.2 seconds to leave building
+    return random() * 0.1 + 1.1
+
+
+def to_nurse():
+    # run up to nurse time interval 3 seconds to 3.1 seconds
+    return random() * 0.1 + 3
+
+
+def paying_attention_break():
+    # timer between 0.25 seconds to 0.50 seconds
+    return random() * 0.25 + 0.25
+
+
+def attack_break():
+    # timer between 32 and 37 seconds for waiting out a horde attack
+    return random() * 5 + 32
+
+
+def starting_battle_break():
+    # timer between 15 and 20 seconds before checking item
+    return random() * 5 + 15
+
+
+def run_away_break():
+    # timer between 2.25 to 5 seconds before inputting
+    return random() * 2.75 + 2.25
+
+
+def heal_up_break():
+    # dialogue of nurse healing break from 9 seconds to 14 seconds
+    return random() * 5 + 9
+
+
+# break for hoping on bike from 0.1 - 0.25 seconds
+def input_break():
+    return random() * 0.15 + 0.1
